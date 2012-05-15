113221a9c38d6c25df45ed80f0d643b544e167bf
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-05-16 00:26:00 +0200

    Represent planes internally with a vector and a offset along that vector.

--- src/plane3.cpp
+++ src/plane3.cpp
@@ -1,24 +1,40 @@
 #include "plane3.h"
-#include "vector3.h"
 #include "ray3.h"
+#include "point3.h"
+#include "vector3.h"
 
 namespace softedge {
 
 Plane3::Plane3() {
 }
 
-Plane3::Plane3(Point3 point, Vector3 normal) :
-        point(point), normal(normal) {
+Plane3::Plane3(Vector3 normal, real d) :
+        normal(normal), d(d) {
+}
+Plane3::Plane3(Vector3 normal, Point3 p) :
+        normal(normal), d(length(p)) {
+}
+
+Plane3::Plane3(Point3 a, Point3 b, Point3 c) :
+        normal(cross(b - a, c - a)), d(dot(normal, a)) {
+
 }
 
 bool Plane3::intersect(const Ray3& ray, real* t, Vector3* normal) const {
-    real d = dot(this->normal, ray.direction);
-    if (d == 0) {
+    real NdD = dot(this->normal, ray.direction);
+    if (NdD == 0) {
         // Plane and ray is parallel.
         return false;
     }
 
-    *t = dot((point - ray.point), this->normal) / d;
+    // Distance from ray origin to plane.
+    real distp = dot(this->normal, ray.point) - d;
+    if (distp < .0) {
+        // Ray starts behind plane.
+        return false;
+    }
+
+    *t = (d - dot(this->normal, ray.point)) / NdD;
     *normal = this->normal;
 
 //    if (d > 0) {
--- src/plane3.h
+++ src/plane3.h
@@ -3,23 +3,25 @@
 
 #include "geometric3.h"
 #include "vector3.h"
-#include "point3.h"
+#include "real.h"
 
 namespace softedge {
 
+class Point3;
 class Ray3;
 
 class Plane3: public Geometric3 {
 public:
     Plane3();
 //    Plane3(Plane3& plane);
-    Plane3(Point3 point, Vector3 normal);
-//    Plane3(Point3 p1, Point3 p2, Point3 p3);
+    Plane3(Vector3 normal, real d);
+    Plane3(Vector3 normal, Point3 p);
+    Plane3(Point3 p1, Point3 p2, Point3 p3);
 
     bool intersect(const Ray3& ray, real* t, Vector3* normal) const;
 
-    Point3 point;
     Vector3 normal;
+    real d;
 };
 
 }  // namespace softedge
