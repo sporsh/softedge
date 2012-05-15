cad855c926bf37d127b2515a4303c8b4124b97bf
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-05-16 00:27:20 +0200

    Improve implementation of ray-sphere intersection.

--- src/sphere.cpp
+++ src/sphere.cpp
@@ -12,29 +12,14 @@ Sphere::Sphere(Point3 origin, real radius) :
 }
 
 bool Sphere::intersect(const Ray3& ray, real* t, Vector3* normal) const {
-    Vector3 diff(origin - ray.point);
-    real d = dot(ray.direction, diff);
-    real incidence = d * d - dot(diff, diff) + radius * radius;
-    if (incidence > .0) {
-        real s = sqrt(incidence);
-        if (s < 0) {
-            d += s;
-        } else {
-            d -= s;
-        }
-        if (d < 0) {
-            return false;
-        }
-        if (t) {
-            *t = d;
-        }
-        if (normal) {
-            *normal = normalize(origin - (ray.point + (ray.direction * d)));
-        }
-        return true;
-    } else {
+    Vector3 dist = origin - ray.point;
+    real B = dot(ray.direction, dist);
+    real D = B * B - dot(dist, dist) + radius * radius;
+    if (D < 0.0) {
         return false;
     }
+    *t = B - sqrt(D);
+    *normal = normalize((ray.point + (ray.direction * *t)) - origin);
+    return true;
 }
 
-} // namespace softedge
