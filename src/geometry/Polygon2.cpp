8f0e1326fd7c44b7e967f5478986a2ca5cc03851
Geir Sporsheim <geir.sporsheim@gmail.com>
2012-05-25 03:54:07 +0200

    Two dimensional generic polygon class.

---
+++ src/geometry/Polygon2.cpp
@@ -0,0 +1,21 @@
+/*
+ * Polygon2.cpp
+ *
+ *  Created on: May 20, 2012
+ *      Author: geir
+ */
+
+#include "Polygon2.h"
+
+namespace softedge {
+
+Polygon2::Polygon2() {
+    // TODO Auto-generated constructor stub
+
+}
+
+Polygon2::~Polygon2() {
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace softedge */
---
+++ src/geometry/Polygon2.h
@@ -0,0 +1,20 @@
+/*
+ * Polygon2.h
+ *
+ *  Created on: May 20, 2012
+ *      Author: geir
+ */
+
+#ifndef POLYGON2_H_
+#define POLYGON2_H_
+
+namespace softedge {
+
+class Polygon2 {
+public:
+    Polygon2();
+    virtual ~Polygon2();
+};
+
+} /* namespace softedge */
+#endif /* POLYGON2_H_ */
